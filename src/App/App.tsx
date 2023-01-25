73ebfa8d4440cf52c840cc900cb021c07cf71ba5
FreeNelu <nelu.maciuca99@gmail.com>
2023-01-25 19:57:21 +0200

    create theme, add materialui, add empty components

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../Components/Header/Header';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       yoo
     </div>
   );
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+function Header() {
+    return (
+        <div className="Header">
+            yoo
+        </div>
+    );
+}
+
+export default Header;
