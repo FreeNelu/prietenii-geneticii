1adb5ae0353cf512e4db279ce2bbf9807f35bd50
FreeNelu <nelu.maciuca99@gmail.com>
2023-01-26 02:34:45 +0200

    customize headere

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import './Header.scss';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
+import '../../Theme/colors.scss';
+import { AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Button, MenuItem } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import logo from './logo.png'
+import LogoIcon from '../CustomIcons/LogoIcon'
 
-const pages = ['About us', 'Donate', 'News'];
+const pages = ['About us', 'News'];
 
 function Header() {
     const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
@@ -26,30 +19,62 @@ function Header() {
     };
 
     return (
-        <AppBar position="static">
+        <AppBar position="static" color='transparent' sx={{ boxShadow: "none" }}>
             <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            color: 'inherit',
-                            textDecoration: 'none',
-                            lineHeight: 1,
-                        }}
-                    >
-                        Prietenii
-                        <br />
-                        Geneticii
-                    </Typography>
+                <Toolbar disableGutters sx={{
+                    display: "flex",
+                    justifyContent: "space-between"
+                }}>
+                    <div className="Logo">
+                        <LogoIcon height={96} />
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            sx={{
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                color: '#E03D8C',
+                                textDecoration: 'none',
+                                lineHeight: 1,
+                                pointerEvents: 'none'
+                            }}
+                        >
+                            Prietenii
+                            <br />
+                            Geneticii
+                        </Typography>
+                    </div>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <Box sx={{
+                        flexGrow: 1, display: {
+                            xs: 'none', md: 'flex', justifyContent: "flex-end"
+                        }
+                    }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: "#01B5E3", fontSize: 16, marginRight: 2, borderRadius: 6 }}
+                            >
+                                {page}
+                            </Button>
+                        ))
+                        }
+                        <Button
+                            key={"Donate"}
+                            onClick={handleCloseNavMenu}
+                            sx={{
+                                my: 2, color: "white", fontSize: 16, backgroundColor: "#69C441", padding: "8px 12px", borderRadius: 6,
+                                ':hover': {
+                                    bgcolor: '#60b43b',
+                                },
+                            }}
+                        >
+                            {"Donate"}
+                        </Button>
+                    </Box>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none', justifyContent: "flex-end" } }}>
                         <IconButton
                             size="large"
                             aria-controls="menu-appbar"
@@ -57,7 +82,7 @@ function Header() {
                             onClick={handleOpenNavMenu}
                             color="inherit"
                         >
-                            <MenuIcon />
+                            <MenuIcon sx={{ fontSize: "2rem" }} />
                         </IconButton>
                         <Menu
                             id="menu-appbar"
@@ -82,42 +107,14 @@ function Header() {
                                     <Typography textAlign="center">{page}</Typography>
                                 </MenuItem>
                             ))}
+                            <MenuItem key={"Donate"} onClick={handleCloseNavMenu}>
+                                <Typography sx={{ color: "#69C441" }} textAlign="center">{"Donate"}</Typography>
+                            </MenuItem>
                         </Menu>
                     </Box>
-
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href=""
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        Prietenii Geneticii
-                    </Typography>
-
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                            <Button
-                                key={page}
-                                onClick={handleCloseNavMenu}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                {page}
-                            </Button>
-                        ))}
-                    </Box>
-
                 </Toolbar>
             </Container>
-        </AppBar>
+        </AppBar >
     );
 }
 
