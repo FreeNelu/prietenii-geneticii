ab00cb4b720b9c184e2e5f85951977193682c7e7
FreeNelu <nelu.maciuca99@gmail.com>
2023-01-27 17:25:26 +0200

    add wave, styles, responsive layout, placeholders

--- src/Components/MainPage/MainPage.styles.tsx
+++ src/Components/MainPage/MainPage.styles.tsx
@@ -1,7 +1,33 @@
+import zIndex from "@mui/material/styles/zIndex";
+import { maxHeight } from "@mui/system";
 import { makeStyles } from "Theme";
 
 export const useStyles = makeStyles()(
     (theme) => ({
+        MainPageContainer: {
+            display: "flex",
+            marginTop: 64,
+        },
+        Wave: {
+            position: "absolute",
+            fill: theme.pink,
+            width: "100%",
+            transform: "rotate(180deg) scaleY(-1) translate(0, -99%)",
 
+        },
+        PageDescription: {
+            float: "left",
+            minWidth: 284,
+            minHeight: 400,
+            flex: 1,
+        },
+        ImageBox: {
+            maxWidth: 400,
+            minWidth: 284,
+            width: "100%",
+            alignSelf: "center",
+            margin: "0px auto",
+            flex: 1,
+        }
     })
 );
--- src/Components/MainPage/MainPage.tsx
+++ src/Components/MainPage/MainPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useStyles } from './MainPage.styles'
 import { Box, useMediaQuery, useTheme } from '@mui/material';
 import girlImage from 'Assets/girlImage.png'
+import Wave from 'Components/Icons/Wave';
 
 type MainPageProps = {
 };
@@ -14,17 +15,32 @@ function MainPage(props: MainPageProps) {
 
 
     return (
-        <div>
-            <Box
-                component="img"
-                sx={{
-                    maxHeight: 800,
-                    maxWidth: 400,
-                }}
-                alt="Girl climbing stairs made out of DNA"
-                src={girlImage}
-            />
-        </div>
+        <>
+            <Box className={classes.MainPageContainer} sx={{
+                flexWrap: {
+                    xs: 'wrap', md: 'nowrap'
+                }
+            }}>
+                <div className={classes.PageDescription} style={{
+                    marginLeft: isMobile ? 0 : "15%",
+                }}>
+                    <h1>Lorem Ipsum</h1>
+                    <h4>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</h4>
+                    <h5>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</h5>
+                </div>
+                <Box
+                    component="img"
+                    sx={{
+                        paddingRight: isMobile ? 0 : "15%",
+                    }}
+                    className={classes.ImageBox}
+                    alt="Girl climbing stairs made out of DNA"
+                    src={girlImage}
+                />
+            </Box >
+            <Wave className={classes.Wave} />
+            <div style={{ width: "100%", height: "500px", backgroundColor: "#E03D8C", }} />
+        </>
     );
 }
 
