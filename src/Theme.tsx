58e3492528901c34bf0ad2a13e57d406e197791b
FreeNelu <nelu.maciuca99@gmail.com>
2023-01-29 17:15:23 +0200

    fix eslint warnings

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -1,18 +1,19 @@
-import { createMakeAndWithStyles } from "tss-react";
+import { createMakeAndWithStyles } from 'tss-react'
 
-function useTheme() {
-    return {
-        textPrimary: "#261425",
-        background: "#EBEFEB",
-        blue: "#01B5E3",
-        pink: "#E03D8C",
-        green: "#69C441",
-        darkgreen: "#60b43b",
-    };
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function useTheme () {
+  return {
+    textPrimary: '#261425',
+    background: '#EBEFEB',
+    blue: '#01B5E3',
+    pink: '#E03D8C',
+    green: '#69C441',
+    darkgreen: '#60b43b'
+  }
 }
 
 export const {
-    makeStyles,
-    withStyles,
-    useStyles
-} = createMakeAndWithStyles({ useTheme });
+  makeStyles,
+  withStyles,
+  useStyles
+} = createMakeAndWithStyles({ useTheme })
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App/App';
-import reportWebVitals from './reportWebVitals';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.scss'
+import App from './App/App'
+import reportWebVitals from './reportWebVitals'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
-reportWebVitals();
+reportWebVitals()
